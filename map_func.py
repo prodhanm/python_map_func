d747a368d2f0def064fd02da4766e94d9ac03251
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-24 13:36:56 -0400

    added exception handling to the code.

--- map_func.py
+++ map_func.py
@@ -2,16 +2,22 @@ names = ["Mufassa Whatta", "Sabina Whatta", "Fazwan Whattta", \
          "Nuaym Whatta"]
 
 def email_join(item):
-    name= item.replace(" ", ".")
-    return name + "@whatta.com"
-        
-def main():
-    email_list = list(map(email_join,names))
-    for email in range(len(email_list)):
-        email_list[email]
+    try:
+        name= item.replace(" ", ".")
+        return name + "@whatta.com"
+    except TypeError as err:
+        print(err)
 
-    dict_name = dict(zip(names,email_list))
-    print(f"email data = {dict_name}")
+def main():
+    try:
+        email_list = list(map(email_join,names))
+        for email in range(len(email_list)):
+            email_list[email]
+
+        dict_name = dict(zip(names,email_list))
+        print(f"email data = {dict_name}")
+    except (KeyError, ValueError) as err:
+        print(err)
 
 if __name__ == "__main__":
     main()
